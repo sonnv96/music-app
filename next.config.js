9bb96ac84b246e5b312c587d56b2fdac5d70fd70
Viet Son <37080650+sonnv96@users.noreply.github.com>
2021-07-18 16:44:00 +0700

    config taiwindcss and import image with module

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  reactStrictMode: true,
+  webpack: (config, options) => {
+    // Important: return the modified config
+    config.module.rules.push({
+      test: /\.(png|jpg|gif)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+          }
+        },
+      ],
+      type: 'javascript/auto'
+    })
+    return config
+  },
 }
